643f2c176d34b285fa5918041895402042ba8c31
Adam Cvikl <adam.wasd4@gmail.com>
2023-05-24 17:38:01 +0200

    spawn an enemy every second

---
+++ Scripts/game.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var enemy_scene = preload("res://Enemies/enemy_square.tscn")
+
+
+func _on_timer_timeout():
+	# Spawn enemy
+	var enemy = enemy_scene.instantiate()
+
+	# Get random direction 0-3
+	var dir: int = randi() % 4
+
+	match dir:
+		# Top
+		0:
+			enemy.position = Vector2(randf_range(-1100, 1100), -550)
+		# Bottom
+		1:
+			enemy.position = Vector2(randf_range(-1100, 1100), 550)
+		# Left
+		2:
+			enemy.position = Vector2(-1100, randf_range(-550, 550))
+		# Right
+		3:
+			enemy.position = Vector2(1100, randf_range(-550, 550))
+
+	add_child(enemy)
+	pass
