dc48fdebcdc59975d97342e9d46cb47250b8322c
Adam Cvikl <adam.wasd4@gmail.com>
2023-05-24 16:43:24 +0200

    rotate the enemy towards the player

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-@export var enemy_speed = 80
+@export var enemy_speed: int = 80
 
 
 func _process(delta: float):
 	# Move the enemy towards the center of the screen (0, 0)
 	position = position.move_toward(Vector2(0, 0), delta * enemy_speed)
+	rotation = Vector2(0, 0).angle_to_point(position)
+
 	pass
