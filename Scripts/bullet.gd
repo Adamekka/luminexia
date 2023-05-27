68227643fcce9656e5d5d562952a9d248ee8314d
Adam Cvikl <adam.wasd4@gmail.com>
2023-05-27 13:43:53 +0200

    fix bullet movement

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -6,10 +6,9 @@ extends Area2D
 const PI_2: float = PI / 2
 
 
-func _physics_process(_delta: float):
+func _physics_process(delta: float):
 	# Move bullet in the direction it is facing
-	velocity = Vector2(speed, 0).rotated(rotation + 3 * PI_2)
-	move_and_slide()
+	position += Vector2(speed * delta, 0).rotated(rotation + 3 * PI_2)
 	# Destroy bullet after TTL
 	await get_tree().create_timer(5).timeout
 	queue_free()
